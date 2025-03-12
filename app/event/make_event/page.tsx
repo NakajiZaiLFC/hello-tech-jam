c18773abbc166d4e299edbfc267960a115b30284
oshiro nonoka <hamster@MacBookPro.lan>
2025-03-12 16:43:05 +0900

    ヘッダーと仮ページ作成しました

---
+++ app/event/make_event/page.tsx
@@ -0,0 +1,24 @@
+
+
+import Layout from '@/components/Layout'  // 追記
+import Link from 'next/link';
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-stone-50">
+      <div className="text-center">
+        <a href="#" className="text-black text-2xl no-underline">
+        イベント作成ページ
+        </a>
+        <div className="mt-5">
+          <Link href="/event/gonzo">
+            <button className="px-4 py-2 text-lg font-semibold bg-blue-500 text-white rounded">
+             コンシェルずへ
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/event/share/page.tsx
@@ -0,0 +1,16 @@
+
+import Layout from '@/components/Layout'  // 追記
+
+
+export default function Home() {
+    return (
+      <div className="flex justify-center items-center h-screen bg-stone-50">
+        <div className="text-center">
+          <a href="#" className="text-black text-2xl no-underline">
+         結果ページ 
+          </a>
+        </div>
+      </div>
+    );
+  }
+  
