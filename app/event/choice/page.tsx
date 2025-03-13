62b44667bead504ba79becf63580cc1b84b02e27
oshiro nonoka <hamster@MacBookPro.lan>
2025-03-13 11:19:33 +0900

    クエリパラメーターを渡しかた指定したよ

--- app/event/choice/page.tsx
+++ app/event/choice/page.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
+// npm install lz-string
+import LZString from "lz-string";
 
 export default function Home() {
+  //apiからurl取得したら、一旦LZString.compressToEncodedURIComponentに入れたやつを
+  //クエリパラメーターとして渡してね
+  //以下は仮です。
+
+  const name = "和風亭";
+  const tel = "08012345678";
+  const lat =35.669220;
+  const lng = 139.761457;
+  const url = "https://columbia.jp/artist-info/nonochan/";
+  const compressedUrl = LZString.compressToEncodedURIComponent(url);
+
   return (
     <div className="flex justify-center items-center h-screen bg-stone-50">
       <div className="text-center">
         <a href="#" className="text-black text-2xl no-underline">
-          店舗取捨選択ぺー時
+          店舗取捨選択ページ
         </a>
         <div className="mt-5">
-          <Link href="/event/share">
+
+          <Link href={`/event/share?url=${compressedUrl}name=${name}tel=${tel}lat=${lat}lng=${lng}`}>
             <button className="px-4 py-2 text-lg font-semibold bg-blue-500 text-white rounded">
               シェアページへ
             </button>
@@ -18,3 +32,4 @@ export default function Home() {
     </div>
   );
 }
+
--- app/event/share/page.tsx
+++ app/event/share/page.tsx
@@ -1,12 +1,32 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import LZString from "lz-string";
+
 export default function Home() {
-    return (
-      <div className="flex justify-center items-center h-screen bg-stone-50">
-        <div className="text-center">
-          <a href="#" className="text-black text-2xl no-underline">
-         結果ページ 
-          </a>
-        </div>
-      </div>
-    );
+  const searchParams = useSearchParams();
+
+  const urlParam = searchParams.get("url");
+  let originalUrl = "";
+  if (urlParam) {
+    originalUrl = LZString.decompressFromEncodedURIComponent(urlParam);
   }
-  
+
+  const name = searchParams.get("name") || "名前がありません";
+  const tel = searchParams.get("tel") || "電話番号がありません";
+  const lat = searchParams.get("lat") || "緯度がありません";
+  const lng = searchParams.get("lng") || "経度がありません";
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-stone-50">
+      <div className="text-center">
+        <p className="text-black text-2xl">
+         圧縮;{urlParam || "リンクがありません"} URL: {originalUrl || "リンクがありません"}
+        </p>
+        <p className="text-black text-2xl">Name: {name}</p>
+        <p className="text-black text-2xl">Tel: {tel}</p>
+        <p className="text-black text-2xl">Lat: {lat}</p>
+        <p className="text-black text-2xl">Lng: {lng}</p>
+      </div>
+    </div>
+  );
+}
